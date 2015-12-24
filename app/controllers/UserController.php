4be10ab27920b19579f3588a6d848ef536be13a6
safari <sshsfl@yeah.net>
2015-12-24 21:19:44 +0800

    test and remove some code init

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -1,29 +1,20 @@
 <?php
 
 use LucaDegasperi\OAuth2Server\Authorizer;
-use Rootant\Api\Exception\ValidationException;
-use Rootant\Api\Exception\DuplicateOperationException;
 
 class UserController extends CommonController
 {
     public function __construct(Authorizer $authorizer)
     {
         parent::__construct($authorizer);
-        $this->beforeFilter('oauth', ['except' => 'store']);
-        $this->beforeFilter('oauth.checkClient', ['only' => 'store']);
-        $this->beforeFilter('validation');
+        // $this->beforeFilter('oauth', ['except' => 'tmp']);
+        // $this->beforeFilter('oauth.checkClient', ['only' => 'store']);
+        // $this->beforeFilter('validation');
     }
 
     private static $_validate = [
-        'store' => [
-            'username' => 'required',
-            'email'    => 'required|email',
-            'password' => 'required|min:6|confirmed',
-        ],
-        'modify' => [
-            'email'  => 'email',
-            'gender' => 'in:男,女',
-        ],
+        // todo
     ];
 
+    // todo
 }
